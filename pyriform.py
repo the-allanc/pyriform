28e67d4854eecf9ec9b3c53e524d8b991211c261
Allan Crooks <allan@increment.one>
2017-08-28 17:31:47 +0100

    Refactor tests, and add support for pass request headers and request body content.

--- pyriform.py
+++ pyriform.py
@@ -24,11 +24,29 @@ class WSGIAdapter(BaseAdapter):
         parsed = urlparse(request.url)
         environ = {'HTTP_HOST': parsed.netloc}
 
+        non_body_methods = set('GET HEAD DELETE OPTIONS'.split())
+        with_body_methods = set('POST PUT PATCH'.split())
+
+        wtparams = dict(headers=request.headers, extra_environ=environ,
+                        url=request.url)
+        if request.method in with_body_methods:
+            wtparams['params'] = request.body
+
+        # Delegate to the appropriate handler if we have one.
+        if request.method in non_body_methods or \
+            request.method in with_body_methods:
+            handler = getattr(self.app, request.method.lower())
+        else:
+            # Sort this out later.
+            raise NotImplementedError(request.method)
+
         # Execute the request.
-        wtresp = self.app.get(request.url, extra_environ=environ)
+        wtresp = handler(**wtparams)
 
         # Convert the response.
         resp = Response()
+        resp.status_code = wtresp.status_code
+        resp.reason = wtresp.status.split(' ')[1]
         resp.url = request.url
 
         for key, value in wtresp.headerlist:
--- tests/test_pyriform.py
+++ tests/test_pyriform.py
@@ -1,13 +1,118 @@
-from httpbin import app as testapp
+from httpbin import app as binapp
 from pyriform import WSGIAdapter
+from requests import Session
 
 
 class TestPyriform(object):
 
-    def test_url(self):
-        adapter = WSGIAdapter(testapp)
-        from requests import Session
+    def setup_class(cls):
+        adapter = WSGIAdapter(binapp)
         sess = Session()
-        sess.mount('http://myapp.local/', adapter)
-        url = 'http://myapp.local/anything/hello.world?how=are+you'
-        assert sess.get(url).json()['url'] == url
+        sess.mount('http://', adapter)
+        sess.mount('https://', adapter)
+
+        cls.session = sess
+
+    def test_url(self):
+
+        # Test that the URL we request is what we end up with.
+        def assert_url(the_url):
+            resp = self.session.get(the_url)
+            resp.raise_for_status()
+            assert resp.json()['url'] == the_url
+
+        # Test with default port on simple URL.
+        assert_url('http://myapp.local/anything/hello.world')
+
+        # Add query string, ensure that works.
+        assert_url('http://myapp2.local/anything/hello.world?how=are+you')
+
+        # Use non-default port.
+        assert_url('http://myapp3.local:17180/anything/hello.world')
+
+        # Use HTTPS - webtest should add the de-facto headers for indicating
+        # HTTPS connections.
+        assert_url('https://myapp4.local/anything/hello.world')
+
+    def test_request_headers(self):
+        headers = {'User-Agent': 'me', 'X-HeyBoy': 'HeyGirl'}
+        resp = self.session.get('http://myapp.local/headers', headers=headers)
+        resp.raise_for_status()
+
+        # Turn this into a case-insensitive dictionary to make it easier to
+        # compare.
+        from requests.utils import CaseInsensitiveDict
+        resp_headers = CaseInsensitiveDict(resp.json()['headers'])
+
+        for h_key, h_value in headers.items():
+            assert resp_headers.get(h_key) == h_value
+
+    def test_post_json(self):
+        url = 'http://myapp2.local/anything/hello.world?how=are+you'
+        resp = self.session.post(url, json=[1, 3])
+        resp.raise_for_status()
+        jresp = resp.json()
+        assert jresp['method'] == 'POST'
+        assert jresp['url'] == url
+        assert jresp['json'] == [1, 3]
+
+    def test_post_form(self):
+        url = 'http://myapp2.local/anything/hello.world?how=are+you'
+        resp = self.session.post(url, data={'ab': 'cd'})
+        resp.raise_for_status()
+        jresp = resp.json()
+        assert jresp['method'] == 'POST'
+        assert jresp['url'] == url
+        assert jresp['form'] == {'ab': 'cd'}
+
+    def test_post_multipart(self):
+        import base64
+        import os
+
+        # We'll send this for a multipart file upload.
+        fpath = os.path.join(os.path.dirname(__file__), 'yellowbg.png')
+        bg_image = open(fpath, 'rb')
+        bg_base64 = base64.b64encode(bg_image.read()).decode('ascii')
+        bg_image.seek(0)
+
+        resp = self.session.post('http://myapp.local/post',
+                                 files={'background': bg_image})
+        resp_files = resp.json()['files']
+        assert 'background' in resp_files
+        assert resp_files['background'] == \
+               'data:application/octet-stream;base64,' + bg_base64
+
+    def test_other_methods(self):
+        pass
+
+    def test_unknown_method(self):
+        pass
+
+    def test_status_code(self):
+        pass
+
+    def test_response_headers(self):
+        pass
+
+    def test_redirect(self):
+        '''
+        /redirect/:n 302 Redirects n times.
+        /redirect-to?url=foo 302 Redirects to the foo URL.
+        /redirect-to?url=foo&status_code=307 307 Redirects to the foo URL.
+        /relative-redirect/:n 302 Relative redirects n times.
+        /absolute-redirect/:n 302 Absolute redirects n times.
+        '''
+        pass
+
+    def test_stream(self):
+        pass
+
+    def test_timeout(self):
+        pass
+
+    def test_environ_headers(self):
+        pass
+
+    def test_files(self):
+        pass
+
